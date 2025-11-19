32d6d544ca08c6580281dede7c74a14583f4caf4
sehooneom04 <sehooneom@dankook.ac.kr>
2025-11-19 16:32:02 +0900

    환경 변수 생성, 카카오 URL 등록

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,45 @@
 import React from "react";
-import '../App.css';
-import './Login.css'
+import "../App.css";
+import "./Login.css";
 
-function Login(){
-    // const REST_API_KEY = "API_KEY";
-    // const REDIRECT_URI = "URI";
+function Login() {
+  const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
+  const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
 
-    const link = 'https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code';
+  const KAKAO_AUTH_URL =
+    "https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code";
+  const NAVER_AUTH_URL =
+    "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}";
 
-    const loginHandler = () => {
-        window.location.href = link;
-    }
-    return (
+  const KakaoLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+  const NaverLogin = () => {
+    window.location.href = NAVER_AUTH_URL;
+  };
 
-
-    <div className="container login-page"> 
+  return (
+    <div className="container login-page">
       {/* container 전체 검정 배경 */}
-            <h1 className="welcome-title font-bold">환영합니다.</h1>
+      <h1 className="welcome-title font-bold">환영합니다.</h1>
 
-            <p className="welcome-subtitle font-semibold">
-            <span className="runpt-text font-semibold">RunPT</span>로<br />
-            스마트한 러닝을 시작해보세요.
-            </p>
+      <p className="welcome-subtitle font-semibold">
+        <span className="runpt-text font-semibold">RunPT</span>로<br />
+        스마트한 러닝을 시작해보세요.
+      </p>
 
-            <img 
-            src="/assets/logo.png" 
-            alt="RunPT Main Logo" 
-            className="main-logo"
-            />
+      <img src="/assets/logo.png" alt="RunPT Main Logo" className="main-logo" />
 
-            <div className="login-buttons">
+      <div className="login-buttons">
+        <button className="login-btn kakao-btn" onClick={KakaoLogin}>
+          <img src="/assets/kakao_phone.png" alt="Kakao" className="icon" />
+        </button>
 
-            <button className="login-btn naver-btn">
-                <img src="/assets/naver_phone.png" alt="Naver" className="icon" />
-            </button>
-
-            <button className="login-btn kakao-btn" onClick={loginHandler}>
-                <img src="/assets/kakao_phone.png" alt="Kakao" className="icon" />
-            </button>
-
-            </div>
-        </div>
+        <button className="login-btn naver-btn" onClick={NaverLogin}>
+          <img src="/assets/naver_phone.png" alt="Naver" className="icon" />
+        </button>
+      </div>
+    </div>
   );
 }
 export default Login;
