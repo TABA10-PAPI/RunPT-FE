58339428de7af5e75b13417623dc5285b110e756
sehooneom04 <sehooneom@dankook.ac.kr>
2025-11-20 10:57:17 +0900

    css 내 스타일 이름 수정, welcome 틀만 잡기(배치 조정 필요)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -21,14 +21,14 @@ function Login() {
   return (
     <div className="container login-page">
       {/* container 전체 검정 배경 */}
-      <h1 className="welcome-title font-bold">환영합니다.</h1>
+      <h1 className="login-title font-bold">환영합니다.</h1>
 
-      <p className="welcome-subtitle font-semibold">
+      <p className="login-subtitle font-semibold">
         <span className="runpt-text font-semibold">RunPT</span>로<br />
         스마트한 러닝을 시작해보세요.
       </p>
 
-      <img src="/assets/logo.png" alt="RunPT Main Logo" className="main-logo" />
+      <img src="/assets/logo.png" alt="RunPT Main Logo" className="login-logo" />
 
       <div className="login-buttons">
         <button className="login-btn kakao-btn" onClick={KakaoLogin}>
